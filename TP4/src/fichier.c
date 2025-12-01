b14b0b6c11b60e1972ffcefc0ef59f98adf93427
patinlucas095-rgb <patinlucas095@gmail.com>
2025-12-01 11:30:08 +0100

    Refactor file reading and writing functions

--- TP4/src/fichier.c
+++ TP4/src/fichier.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fichier.h"
 
+// Fonction pour lire le contenu d'un fichier
 void lire_fichier(const char *nom_de_fichier) {
     FILE *f = fopen(nom_de_fichier, "r");
-
     if (f == NULL) {
-        printf("Erreur : impossible d'ouvrir le fichier %s\n", nom_de_fichier);
+        printf("Erreur : impossible d'ouvrir le fichier %s.\n", nom_de_fichier);
         return;
     }
 
-    printf("Contenu du fichier %s :\n", nom_de_fichier);
-
     char c;
+    printf("Contenu du fichier %s :\n", nom_de_fichier);
     while ((c = fgetc(f)) != EOF) {
         putchar(c);
     }
+    printf("\n");
 
     fclose(f);
 }
 
+// Fonction pour écrire un message dans un fichier
 void ecrire_dans_fichier(const char *nom_de_fichier, const char *message) {
     FILE *f = fopen(nom_de_fichier, "w");
-
     if (f == NULL) {
-        printf("Erreur : impossible d'écrire dans le fichier %s\n", nom_de_fichier);
+        printf("Erreur : impossible d'ouvrir le fichier %s.\n", nom_de_fichier);
         return;
     }
 
-    fputs(message, f);
+    fprintf(f, "%s", message);
     fclose(f);
 
     printf("Le message a été écrit dans le fichier %s.\n", nom_de_fichier);
 }
+
