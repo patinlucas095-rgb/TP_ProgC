3b4b5b9548639e3e622a6001e44072e286cc4b01
patinlucas095-rgb <patinlucas095@gmail.com>
2025-12-01 11:18:34 +0100

    Implement basic calculator functionality
    Added a basic calculator that performs various operations based on user input.

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include "operator.h"
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op); // espace avant %c pour ignorer les retours à la ligne
+
+    int resultat = 0;
+
+    switch (op) {
+        case '+':
+            resultat = somme(num1, num2);
+            break;
+        case '-':
+            resultat = difference(num1, num2);
+            break;
+        case '*':
+            resultat = produit(num1, num2);
+            break;
+        case '/':
+            resultat = quotient(num1, num2);
+            break;
+        case '%':
+            resultat = modulo(num1, num2);
+            break;
+        case '&':
+            resultat = et(num1, num2);
+            break;
+        case '|':
+            resultat = ou(num1, num2);
+            break;
+        case '~':
+            resultat = negation(num1, num2);
+            break;
+        default:
+            printf("Operateur invalide.\n");
+            return 1;
+    }
+
+    printf("Résultat : %d\n", resultat);
+    return 0;
+}
 
